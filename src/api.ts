077b6aa38a461397e16d1b788aaf35e6f4787aab
mykola2312 <nikolay_tihonov@i.ua>
2024-01-20 17:38:58 +0200

    implement User interface

--- src/api.ts
+++ src/api.ts
@@ -5,3 +5,9 @@ export const API_BASE_URL = "http://localhost:4444";
 export const API = axios.create({
     baseURL: API_BASE_URL
 });
+
+export interface User {
+    id: number
+    email: string
+    name: string
+}
