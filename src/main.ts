e7a361e89eb36d7a9c3def42d0725261e99a7f53
mykola2312 <nikolay_tihonov@i.ua>
2024-01-20 17:31:46 +0200

    add pinia and begin implementing user store

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,10 @@ import App from './App.vue'
 import ColorPicker from '@radial-color-picker/vue-color-picker';
 import '@radial-color-picker/vue-color-picker/dist/vue-color-picker.min.css';
 
+import { createPinia } from 'pinia';
+
 const app = createApp(App);
 
 app.use(ColorPicker);
+app.use(createPinia());
 app.mount('#app');
---
+++ src/stores/user.ts
@@ -0,0 +1,18 @@
+import { defineStore } from "pinia";
+
+interface UserState {
+}
+
+export const useUserStore = defineStore('user', {
+    state: (): UserState => {
+        return {}
+    },
+
+    getters: {
+
+    },
+
+    actions: {
+
+    }
+});
