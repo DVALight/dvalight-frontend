4ccad2fd8691ed942994a9075c7597a8ef640d6b
mykola2312 <nikolay_tihonov@i.ua>
2024-01-20 17:50:39 +0200

    setup eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  env: {
+    node: true,
+  },
+  root: true,
+  ignorePatterns: ['.eslintrc.js'],
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/vue3-recommended',
+    'prettier',
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  }
+}
