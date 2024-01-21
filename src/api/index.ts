cf44373fb6945c1d251014daf366767d79b51cb7
mykola2312 <nikolay_tihonov@i.ua>
2024-01-21 23:23:50 +0200

    implement refresh token request for axios-jwt

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,4 +1,6 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
+import { IAuthTokens, TokenRefreshRequest, applyAuthTokenInterceptor, getBrowserLocalStorage } from "axios-jwt";
+import { Tokens } from "./auth";
 
 export const API_BASE_URL = "http://localhost:4444";
 
@@ -15,3 +17,21 @@ export interface APIError {
 export function isAPIError(data: any): data is APIError {
   return ("message" in data && "error" in data && "statusCode" in data);
 }
+
+const requestRefresh: TokenRefreshRequest = async (refreshToken: string): Promise<IAuthTokens | string> => {
+  const res = await axios.post<{}, AxiosResponse<Tokens>>(`${API_BASE_URL}/auth/refresh`, {}, {
+    headers: {
+      "Authorization": "Refresh " + refreshToken
+    }
+  });
+
+  return {
+    accessToken: res.data.accesseToken,
+    refreshToken: res.data.refreshToken
+  };
+}
+
+applyAuthTokenInterceptor(API, { requestRefresh });
+
+const getStorage = getBrowserLocalStorage;
+applyAuthTokenInterceptor(API, { requestRefresh, getStorage });
